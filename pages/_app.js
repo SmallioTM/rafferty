9332e46ec9ff9bbffaf1621912e9fd1b540a5bfe
SmallioTM <127532010+SmallioTM@users.noreply.github.com>
2023-09-29 22:20:26 +0300

    Working B4 GSAP

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
-"use client";
+const React = require("react");
+const gsap = require("gsap").gsap;
+const ScrollTrigger = require("gsap").ScrollTrigger;
 
-// pages/_app.js
-import React, { useEffect } from "react";
-import "../src/app/globals.css";
-import Lenis from "@studio-freight/lenis";
+// Register the ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const boxRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const tl = gsap.timeline();
+
+    // Expand the blue box to cover the entire viewport
+    tl.to(boxRef.current, {
+      height: "100vh",
+      duration: 0.3,
+    });
+
+    // Contract the blue box to reveal the next page
+    tl.to(boxRef.current, {
+      height: "0vh",
+      duration: 0.3,
+      delay: 0.2,
+    });
+  }, []);
+
+  return (
+    <>
+      <Component {...pageProps} />
+      <div className="blue-box" ref={boxRef}></div>
+    </>
+  );
 }
 
-export default MyApp;
+module.exports = MyApp;

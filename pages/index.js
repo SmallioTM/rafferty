fa415c896ab4344970028692f4da168d79cc2272
SmallioTM <127532010+SmallioTM@users.noreply.github.com>
2023-09-29 19:52:12 +0300

    Update

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from "../src/app/components/HeroSection/HeroSection";
 import NavBar from "../src/app/components/NavBar/NavBar";
 import "src/app/globals.css";
+import AboutSection from "@/app/components/AboutSection/AboutSection";
 
 export default function Page() {
   // No need to re-initialize Lenis here
@@ -10,6 +11,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
       <NavBar />
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }

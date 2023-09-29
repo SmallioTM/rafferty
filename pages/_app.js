491103f60da77e474815f1c0da75b629cb934236
SmallioTM <127532010+SmallioTM@users.noreply.github.com>
2023-09-29 19:49:41 +0300

    No Errors / lennis not working

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 // pages/_app.js
+import React from "react"; // <-- Import React
 import "../src/app/globals.css";
 import Lenis from "@studio-freight/lenis";
 
@@ -6,10 +7,13 @@ function MyApp({ Component, pageProps }) {
   // Initialize Lenis when the component mounts
   React.useEffect(() => {
     const lenisInstance = new Lenis({
-      // Your configuration here
+      wrapper: window, // The element that will be used as the scroll container.
+      content: document.documentElement, // The element that contains the content that will be scrolled.
+      lerp: 0.1, // Linear interpolation (lerp) intensity (between 0 and 1).
+      orientation: "vertical", // The orientation of the scrolling. Can be 'vertical' or 'horizontal'.
     });
 
-    // Optional: If you want to make the instance globally available
+    // Make the instance globally available
     window.lenisInstance = lenisInstance;
   }, []);
 

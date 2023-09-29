7c71d60607fcee96b1f0b3560bae094a755eaef7
SmallioTM <127532010+SmallioTM@users.noreply.github.com>
2023-09-29 20:23:35 +0300

    not working 5

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,25 @@
 // pages/_app.js
-import React from "react";
+import React, { useEffect } from "react";
 import "../src/app/globals.css";
 import Lenis from "@studio-freight/lenis";
 
 function MyApp({ Component, pageProps }) {
-  // Initialize Lenis when the component mounts
-  React.useEffect(() => {
+  useEffect(() => {
+    // This code will only run on the client side
     const wrapperElement = document.querySelector(".l-wrapper");
     const contentElement = document.querySelector(".l-content");
 
-    const lenisInstance = new Lenis({
-      wrapper: wrapperElement,
-      content: contentElement,
-      lerp: 0.1,
-      orientation: "vertical",
-    });
+    if (wrapperElement && contentElement) {
+      const lenisInstance = new Lenis({
+        wrapper: wrapperElement,
+        content: contentElement,
+        lerp: 0.1,
+        orientation: "vertical",
+      });
 
-    // Make the instance globally available
-    window.lenisInstance = lenisInstance;
+      // Make the instance globally available
+      window.lenisInstance = lenisInstance;
+    }
   }, []);
 
   return <Component {...pageProps} />;

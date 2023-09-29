e9f49bfac4db2ec5559422187bbd13847274a7a0
SmallioTM <127532010+SmallioTM@users.noreply.github.com>
2023-09-29 19:14:55 +0300

    new

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import "public/images/logo.png";
 
 const NavBar = () => {
   return (
     <div className="fixed top-0 w-full h-[70px] bg-grey flex justify-between items-center pr-[40px] pl-[40px]">
       <div className="w-[150px] h-8 flex flex items-center justify-start">
-        <img className="w-[120px] h-[30] flex" src="public/images/logo.png" />
+        <img className="w-[120px] h-[30] flex" src="/logo.png" />
       </div>
       <div className="w-[320px] h-16 flex justify-between items-center px-5">
         <Link href="/" className="text-white">

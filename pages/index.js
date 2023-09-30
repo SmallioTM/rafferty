0139f35993e61da5c2151fce1899d276c2be43b4
SmallioTM <127532010+SmallioTM@users.noreply.github.com>
2023-09-30 21:00:18 +0300

    Scroll Smoother Working

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import "src/app/globals.css";
 
-import React, { useEffect } from "react"; // <-- Import useEffect
+import React, { useEffect } from "react";
 import { gsap } from "libs/gsap.js";
 import { ScrollTrigger } from "libs/ScrollTrigger.min.js";
 import { ScrollSmoother } from "libs/ScrollSmoother.min.js";
@@ -8,26 +8,19 @@ import HeroSection from "../src/app/components/HeroSection/HeroSection";
 import NavBar from "../src/app/components/NavBar/NavBar";
 import AboutSection from "src/app/components/AboutSection/AboutSection.jsx";
 import LargeProjectSection from "src/app/components/LargeProjectSection/LargeProjectSection.jsx";
-import "src/app/globals.css";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function Page() {
   useEffect(() => {
-    let container = document.querySelector("#smooth-content");
+    // Normalize scroll
+    ScrollTrigger.normalizeScroll(true);
 
-    gsap.to(container, {
-      y: (i, target) =>
-        -(target.scrollHeight - document.documentElement.clientHeight),
-      ease: "none",
-      scrollTrigger: {
-        trigger: "#smooth-wrapper",
-        pin: true,
-        scrub: 1,
-        end: () =>
-          `+=${container.scrollHeight - document.documentElement.clientHeight}`,
-        invalidateOnRefresh: true,
-      },
+    // Create the scroll smoother
+    let smoother = ScrollSmoother.create({
+      smooth: 1.6,
+      effects: true,
+      normalizeScroll: true,
     });
   }, []);
 
@@ -38,7 +31,7 @@ export default function Page() {
     >
       <div
         id="smooth-content"
-        className="flex min-h-screen flex-col items-center"
+        className="overflow-hidden flex min-h-screen flex-col items-center"
       >
         <NavBar />
         <HeroSection data-speed="1.2" />
